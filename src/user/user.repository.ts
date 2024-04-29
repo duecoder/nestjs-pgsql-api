3349b2580ec27e88cb2734ae3e64164ff6dfb032
duecoder <eduardodecastro@outlook.com>
2024-04-28 22:25:44 -0300

    feat: user repository

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,10 +1,50 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
-import { User } from './domain/user.entity';
+import { User } from './user.entity';
+import { CreateUserDto } from 'src/common/dto/CreateUserDto';
+import { UserRole } from 'src/domain/UserRole';
+import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(
+    createUserDto: CreateUserDto,
+    role: UserRole,
+  ): Promise<User> {
+    const { email, name, password } = createUserDto;
+
+    const user = new User();
+    user.email = email;
+    user.name = name;
+    user.role = role;
+    user.status = true;
+    user.confirmationToken = crypto.randomBytes(32).toString('hex');
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
+    try {
+      const savedUser = await this.save(user); 
+      delete savedUser.password;
+      delete savedUser.salt;
+      return savedUser;
+    } catch (error) {
+      if (error.code.toString() === '23505') {
+        throw new ConflictException('Endereço de email já está em uso');
+      } else {
+        throw new InternalServerErrorException(
+          'Erro ao salvar o usuário no banco de dados'
+        );
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
